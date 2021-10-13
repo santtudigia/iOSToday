8b02e4d021b4c7aaa325f21a75de389906e782df
Santtu Hyvärinen <santtu.hyvarinen@digia.com>
2021-10-13 16:51:53 +0300

    Initial Commit

---
+++ Today/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Today
+//
+//  Created by Hyvärinen Santtu on 13.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
